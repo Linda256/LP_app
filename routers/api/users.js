c1015e79fce50fdbc76e8e1b8e3dc912dfc39b9d
Linda Wang <lindawang256@gmail.com>
2018-10-24 09:39:37 -0700

    Add user login

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 //Load Register validation
 const validateRegisterInput = require('../../validation/register');
+//Load Login validation
+const validateLoginInput = require('../../validation/login');
 
 // Load User schema
 const User = require('../../models/User');
+const keys = require('../../config/keys');
+
 
 // @route   POST api/users/register
 // @desc    Register users route
@@ -45,6 +51,54 @@ router.post('/register',(req,res)=>{
           })
         }
       })
+})
+
+// @route   POST api/users/login
+// @desc    Login User / Returning Token
+// @access  Public
+router.post('/login', (req,res) =>{
+  const email=req.body.email;
+  const password = req.body.password;
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+
+  User.findOne({email}).then(user=>{
+    //Validate email and password
+    if (!isValid)
+      return res.status(404).json(errors)
+    //Check for user
+    if(!user){
+      errors.email='User not found';
+      return res.status(404).json(errors);
+    }
+    //Check password
+    bcrypt.compare(password,user.password).then(isMatch => {
+      if(isMatch){
+        //User Matched
+        const payload={
+          id:user.id,
+          name:user.name,
+          //avatar:user.avatar
+        }
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {expiresIn:3600 },
+          (err,token)=>{
+            res.json({
+              success:true,
+              token: 'Bearer '+token
+            })}
+          )}
+        else {
+          errors.password = 'Passwrod incorrect';
+        return res.status(400).json(errors)
+      }
+    })
+
+  })
 })
 
 
