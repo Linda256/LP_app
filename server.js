5c8cf8a58732be9bab2ff5bfc0b5f522e386f388
Linda Wang <lindawang256@gmail.com>
2018-10-24 09:41:55 -0700

    Add passport

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users  = require('./routers/api/users');
+const profile = require('./routers/api/profile');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Good Afternoon! Linda!')
 });
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -16,9 +25,14 @@ mongoose
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.post('/register', (req,res) =>{
+//Passport middleware
+app.use(passport.initialize());
 
-})
+//Passport config
+require('./config/passport')(passport);
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
 
 const PORT = process.env.PORT || 5000;
 
