87e4cba9caeb630dbba06213e907559377ec96cf
Linda Wang <lindawang256@gmail.com>
2018-10-24 12:47:47 -0700

    Add client side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import login from './components/auth/Login';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar />
-      <Landing />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component = { Landing } />
+          <div className= "container">
+            <Router exact path='/register' component = { Register } />
+            <Router exact path='/login' component = { login } />
+          </div>
+        </div>
+      </Router>
 
-      </div>
+
     );
   }
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
+        <div className="dark-overlay landing-inner text-dark">
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">LP_APP</h1>
+                <h1 className="display-3 mb-4">LP App</h1>
                 <p className="lead">
                   {' '}
                   A simple profile APP
                 </p>
                 <hr />
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
